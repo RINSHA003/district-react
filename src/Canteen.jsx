5f3e0bf1ba57df3f7554afdba24c544d2cf1dbd0
amelamelame <amelshab2018@gmail.com>
2023-06-15 17:20:16 +0530

    first commit

---
+++ src/Canteen.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Canteen() {
+  return (
+    <div> collage has a Canteen</div>
+  )
+}
---
+++ src/Library.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Library() {
+  return (
+    <div> collage has a very good Library</div>
+  )
+}
